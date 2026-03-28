81bb8a95ffe2503598f78f314c5a29e3c20d252a
Maeonetta <maeonetta@gmail.com>
2026-03-28 16:23:24 -0500

    dontdoxmepls

---
+++ chaptur2.py
@@ -0,0 +1,13 @@
+#Write a program that does the following:
+#Display
+#Name
+#Address
+#phone number
+#college major
+
+my_name = "Mabel"
+home = "1220 Spring Ct"
+phone = "5312228654"
+major = "Computer Science"
+
+print (f"My name is {my_name}\nMy home is {home}\nMy phone is {phone}\nMy major is {major}")
